ad4f5939d1a0a030498f00b3c19c76f1f2781504
MrPigletMan <85534152+MrPigletMan@users.noreply.github.com>
2021-08-12 09:23:32 +1000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,91 @@
 def on_button_pressed_a():
-    global Watch, Stop
-    basic.clear_screen()
-    Watch = 0
-    Stop = 1
+    global Start, StartTime, Clock
+    Start = 1
+    StartTime = input.running_time()
+    Clock = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    global Stop
+def on_button_pressed_ab():
+    global Clock, StartTime, Time, Start
     basic.clear_screen()
-    Stop = 2
+    Clock = 0
+    basic.show_leds("""
+        . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                . . . . .
+    """)
+    StartTime = 0
+    Time = 0
+    Start = 0
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global Clock, Time
+    basic.clear_screen()
+    Clock = 0
+    if 0 == Start:
+        basic.show_leds("""
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        # . . . #
+        """)
+        control.wait_micros(1000000)
+        basic.clear_screen()
+    else:
+        Time = input.running_time() - StartTime
+        basic.show_number(Time / 1000)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-Watch = 0
-Stop = 0
-Stop = 0
-Watch = 0
+def on_gesture_shake():
+    pass
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+Time = 0
+StartTime = 0
+Start = 0
+Clock = 0
+basic.clear_screen()
+Clock = 0
+Start = 0
+StartTime = 0
+Time = 0
 
 def on_forever():
-    global Watch
-    while Stop + 0 == 1:
-        control.wait_micros(1000)
-        Watch += 1
-    if Stop + 0 == 2:
-        basic.show_string("" + str((Watch / 100)))
+    if 1 == Clock:
+        basic.show_leds("""
+            . # # # .
+                        # . # . #
+                        # . # . #
+                        # . . # #
+                        . # # # .
+        """)
+        control.wait_micros(100000)
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # . # # #
+                        # . . # #
+                        . # # # .
+        """)
+        control.wait_micros(100000)
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # . # . #
+                        # . # # #
+                        . # # # .
+        """)
+        control.wait_micros(100000)
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # # # . #
+                        # . . # #
+                        . # # # .
+        """)
+        control.wait_micros(100000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,39 +1,46 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     Start = 1
     StartTime = input.runningTime()
     Clock = 1
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    basic.clearScreen()
+    Clock = 0
+    basic.showLeds(`
+        . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                . . . . .
+    `)
+    StartTime = 0
+    Time = 0
+    Start = 0
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     basic.clearScreen()
     Clock = 0
     if (0 == Start) {
         basic.showLeds(`
             # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+                        . # . # .
+                        . . # . .
+                        . # . # .
+                        # . . . #
+        `)
         control.waitMicros(1000000)
         basic.clearScreen()
     } else {
         Time = input.runningTime() - StartTime
         basic.showNumber(Time / 1000)
     }
+    
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    Clock = 0
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        # . # . .
-        . # . . .
-        . . . . .
-        `)
-    StartTime = 0
-    Time = 0
-    Start = 0
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
 })
 let Time = 0
 let StartTime = 0
@@ -44,39 +51,40 @@ Clock = 0
 Start = 0
 StartTime = 0
 Time = 0
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (1 == Clock) {
         basic.showLeds(`
             . # # # .
-            # . # . #
-            # . # . #
-            # . . # #
-            . # # # .
-            `)
+                        # . # . #
+                        # . # . #
+                        # . . # #
+                        . # # # .
+        `)
         control.waitMicros(100000)
         basic.showLeds(`
             . # # # .
-            # . . . #
-            # . # # #
-            # . . # #
-            . # # # .
-            `)
+                        # . . . #
+                        # . # # #
+                        # . . # #
+                        . # # # .
+        `)
         control.waitMicros(100000)
         basic.showLeds(`
             . # # # .
-            # . . . #
-            # . # . #
-            # . # # #
-            . # # # .
-            `)
+                        # . . . #
+                        # . # . #
+                        # . # # #
+                        . # # # .
+        `)
         control.waitMicros(100000)
         basic.showLeds(`
             . # # # .
-            # . . . #
-            # # # . #
-            # . . # #
-            . # # # .
-            `)
+                        # . . . #
+                        # # # . #
+                        # . . # #
+                        . # # # .
+        `)
         control.waitMicros(100000)
     }
+    
 })
