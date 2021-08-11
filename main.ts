96393d63799460e587c1f486159c2004e0479fa8
MrPigletMan <85534152+MrPigletMan@users.noreply.github.com>
2021-08-12 09:22:22 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,82 @@
 input.onButtonPressed(Button.A, function () {
+    Start = 1
     StartTime = input.runningTime()
-    basic.showIcon(IconNames.SmallDiamond)
+    Clock = 1
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
-    Time = input.runningTime() - StartTime
-    basic.showNumber(Time / 1000)
+    Clock = 0
+    if (0 == Start) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        control.waitMicros(1000000)
+        basic.clearScreen()
+    } else {
+        Time = input.runningTime() - StartTime
+        basic.showNumber(Time / 1000)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
+    Clock = 0
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
     StartTime = 0
     Time = 0
+    Start = 0
 })
 let Time = 0
 let StartTime = 0
+let Start = 0
+let Clock = 0
 basic.clearScreen()
+Clock = 0
+Start = 0
 StartTime = 0
 Time = 0
 basic.forever(function () {
-	
+    if (1 == Clock) {
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            # . # . #
+            # . . # #
+            . # # # .
+            `)
+        control.waitMicros(100000)
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . # # #
+            # . . # #
+            . # # # .
+            `)
+        control.waitMicros(100000)
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . # . #
+            # . # # #
+            . # # # .
+            `)
+        control.waitMicros(100000)
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # # # . #
+            # . . # #
+            . # # # .
+            `)
+        control.waitMicros(100000)
+    }
 })
